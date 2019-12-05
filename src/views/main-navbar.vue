ffc2c2c0f2a15fe7433d0d296d37a064f7faf5b7
tututu <289911401@qq.com>
2019-12-05 17:05:39 +0800

    test

--- src/views/main-navbar.vue
+++ src/views/main-navbar.vue
@@ -3,7 +3,7 @@
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
         <a class="site-navbar__brand-lg" href="javascript:;">APP分发平台</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">人人</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">人 人</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
